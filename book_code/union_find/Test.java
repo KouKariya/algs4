bd8e571fcc64d1e9c9ed32b194325b1bb584c7c0
KouKariya <jssgallarzo@gmail.com>
2017-01-26 21:33:07 -0600

    Uploading code.

---
+++ book_code/union_find/Test.java
@@ -0,0 +1,11 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Test{
+
+	public static void main(String[] args){
+		
+		int n = StdIn.readInt();
+		StdOut.println(n);
+	}
+}
